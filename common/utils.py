e813b8075d73fa6cba643636c3972ba6f38d9e85
Ohad Doron <ogdoron@gmail.com>
2021-07-25 14:47:08 +0300

    added get series uids

--- common/utils.py
+++ common/utils.py
@@ -47,9 +47,23 @@ def get_unique_patient_barcodes(col: pymongo.collection.Collection):
     """
     Fetches unique patient barcode values for a specific collection
 
-    :param col: databae collection handle
+    :param col: database collection handle
     :type col: pymongo.collection.Collection
     :return: list of unique barcode names
-    :rtype: tp.giList[str]
+    :rtype: tp.List[str]
     """
     return col.find({}).distinct("bcr_patient_barcode")
+
+
+def get_series_uids(col: pymongo.collection.Collection, patient_barcode: str):
+    """
+    Fetches uids for a specific patient barcode
+
+    :param col: database collection handle
+    :type col: pymongo.collection.Collection
+    :param patient_barcode: [unique identifier for a patient
+    :type patient_barcode: str
+    :return: series uids matching specific dicom images
+    :rtype: tp.List[str]
+    """
+    return col.find({"bcr_patient_barcode": patient_barcode}).distinct("series_uid")
