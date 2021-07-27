28117876663ec71c9a4dbaac43e73878019a628d
Ohad Doron <ogdoron@gmail.com>
2021-07-27 17:13:50 +0300

    update

--- setup.py
+++ setup.py
@@ -9,12 +9,16 @@ with open('requirements.txt') as f:
 if torch.cuda.is_available():
     reqs = [req for req in reqs if 'torch' not in req]
     reqs += ['torch==1.9.0+cu111', 'torchvision==0.10.0+cu111', 'torchaudio==0.9.0']
+else:
+    reqs += ['torch', 'torchvision', 'torchaudio']
 
 setup(
     name='common',
     version='1.0',
     packages=['tests', 'common'],
-    install_requires=reqs,
+    install_requires=['loguru==0.5.3', 'mongoengine==0.23.1', 'mongomock==3.23.0', 'numpy==1.21.1', 'Pillow==8.3.1',
+                      'pymongo==3.12.0', 'pytest==6.2.4', 'toml==0.10.2', 'torch==1.9.0', 'torchvision==0.10.0',
+                      'trains==0.16.4', 'pytorch-lightning', 'simpleitk', 'lightning-bolts'],
     url='',
     license='',
     author='ohad',
