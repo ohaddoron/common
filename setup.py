71b08f761ef081269bde3c54c16c7bcbd978e38f
Ohad Doron <ogdoron@gmail.com>
2021-07-27 17:36:13 +0300

    setup updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='common',
     version='1.0',
     packages=['tests', 'common'],
-    dependency_links=['https://download.pytorch.org/whl/torch_stable.html'],
+    dependency_links=['https://github.com/pytorch/pytorch'],
     install_requires=['loguru==0.5.3', 'mongoengine==0.23.1',
                       'mongomock==3.23.0', 'numpy==1.21.1', 'Pillow==8.3.1',
                       'pymongo==3.12.0', 'pytest==6.2.4', 'toml==0.10.2', 'torch==1.9.0+cu111',
