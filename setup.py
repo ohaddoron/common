f59c7950ecae257f8bde5c6465221d1facede977
Ohad Doron <ogdoron@gmail.com>
2021-07-27 15:41:41 +0300

    created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='common',
+    version='1.0',
+    packages=['tests', 'common'],
+    url='',
+    license='',
+    author='ohad',
+    author_email='ogdoron@gmail.com',
+    description='Common Utilities'
+)
