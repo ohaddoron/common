38cd39c002d2ca4232908e398f3a645f766f345c
Ohad Doron <ogdoron@gmail.com>
2021-12-12 21:22:59 +0200

    created client to utilize fetcher easier

---
+++ common/fetcher_client.py
@@ -0,0 +1,39 @@
+import httpx
+import typing as tp
+from loguru import logger
+from pydantic import validate_arguments
+
+ADDR = 'http://medical001-5.tau.ac.il/api'
+
+
+def _perform_request(method: str, **params):
+    with httpx.Client(timeout=None) as client:
+        req = getattr(client, method)
+
+        r = req(**params)
+        r.raise_for_status()
+    return r.json()
+
+
+@validate_arguments
+def get_features_for_patients(col: str, feature_name: str, patients: tp.List[str] = None):
+    return _perform_request(method='post', url=f'{ADDR}/features_for_patients',
+                            json=dict(col=col, feature_name=feature_name, patients=patients)
+                            )
+
+
+@validate_arguments
+def get_patients_by_mutation(mutation: str, mutation_status: bool):
+    return _perform_request(method='get', url=f'{ADDR}/patients_by_mutation',
+                            params=dict(mutation=mutation, mutation_status=mutation_status)
+                            )
+
+
+@validate_arguments
+def get_patients_age(patients: tp.List[str]):
+    return _perform_request(method='post', url=f'{ADDR}/patients_age', json=patients)
+
+
+@validate_arguments
+def get_feature_names(col: str):
+    return _perform_request('get', url=f'{ADDR}/feature_names', params=dict(col=col))
